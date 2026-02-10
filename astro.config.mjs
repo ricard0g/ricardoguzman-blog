a0edc312e23e8f7e47537e2002c0d57fc29bafc6
ricard0g <ricardoguzman444@gmail.com>
2026-02-10 18:04:18 +0100

    feat: add search boxfor blog post filtering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -15,5 +16,5 @@ export default defineConfig({
 		plugins: [tailwindcss()],
 	},
 	site: "https://blog.ricardoguzman.dev",
-	integrations: [sitemap()],
+	integrations: [sitemap(), react()],
 });
